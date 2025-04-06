03d3845351da39a46fb1879440a522bba3bd1c50
Zane George <146121353+Zane-Dev14@users.noreply.github.com>
2025-04-06 11:54:21 +0530

    Aight, reversed string

---
+++ tcpClient.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int port=8000;
+int buf=1024;
+
+int main(){
+    int sock;
+    struct sockaddr_in server_addr,client_addr;
+    char buffer[buf];
+    socklen_t addr_size;
+
+    printf("Enter message to send: \n");
+    scanf("%s",buffer);
+    sock=socket(AF_INET,SOCK_STREAM,0);
+    if (sock<0)printf("Failed to connect");
+    server_addr.sin_port=htons(8000);
+    server_addr.sin_family=AF_INET;
+    inet_pton(AF_INET,"127.0.0.1",&server_addr.sin_addr);
+   
+    connect(sock,(struct sockaddr *)&server_addr,sizeof(server_addr));
+    send(sock,buffer,buf,0);
+    buffer[buf]='\0';
+    recv(sock,buffer,buf,0);
+    printf("From server: %s",buffer);
+    
+}
---
+++ tcpServer.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int port=8000;
+int buf=1024;
+
+int main(){
+    struct sockaddr_in server_addr,client_addr;
+    int sock,client_sock;
+    socklen_t addr_size;
+    char buffer[buf];
+    char buffer2[buf];
+    sock=socket(AF_INET,SOCK_STREAM,0);
+    if(sock<0){printf("No socket");}else{printf("Sock");}
+    server_addr.sin_family=AF_INET;
+    server_addr.sin_port=htons(port);
+    server_addr.sin_addr.s_addr=INADDR_ANY;
+    
+    if (bind(sock,(struct sockaddr *)&server_addr,sizeof(server_addr))<0){printf("Could not bind");}else{printf("Binded socket at port %d.\n",port);}
+    addr_size=sizeof(client_addr);
+    
+    listen(sock,5);
+    client_sock=accept(sock,(struct sockaddr *)&client_addr,&addr_size);
+    read(client_sock,buffer,buf);
+    printf("Buffer: %s. Will reverse. \n",buffer);
+    int j=0;
+    for(int i=strlen(buffer)-1;i>=  0;i--){
+        buffer2[j]=buffer[i];
+        j++;
+    }buffer2[j]='\0';
+    send(client_sock,buffer2,strlen(buffer2),0);
+}
